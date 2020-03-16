e44ac3bca415926c576979d4bb27f9d14d14d353
Podkovyrov Denis <denis.podkovyrov@gmail.com>
2020-03-16 17:33:32 +0300

    first impl Map<String, String>

--- src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
+++ src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
@@ -1,70 +1,125 @@
 package ru.gnkoshelev.kontur.intern.redis.map;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Pipeline;
+
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * @author Gregory Koshelev
- */
 public class RedisMap implements Map<String,String> {
+    private JedisPool jedisPool = new JedisPool();
+    private final String MAP_ID = "redisMap:";
+    private String mapId;
+
+    public RedisMap(){
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.incr("mapId");
+            this.mapId = MAP_ID + jedis.get("mapId");
+            jedis.hincrBy("usedMap", mapId, 1);
+        }
+    }
+
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.hlen(mapId).intValue();
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return size() == 0;
     }
 
     @Override
     public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()){
+            return jedis.hkeys(mapId).contains(key);
+        }
     }
 
     @Override
     public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()){
+            return jedis.hkeys(mapId).contains(value);
+        }
     }
 
     @Override
     public String get(Object key) {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.hget(mapId, (String) key);
+        }
     }
 
     @Override
     public String put(String key, String value) {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()) {
+            return String.valueOf(jedis.hset(mapId, key, value));
+        }
     }
 
     @Override
     public String remove(Object key) {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()) {
+            return String.valueOf(jedis.hdel(mapId, (String) key));
+        }
     }
 
     @Override
     public void putAll(Map<? extends String, ? extends String> m) {
-        throw new UnsupportedOperationException();
+       try (Jedis jedis = jedisPool.getResource()) {
+           Pipeline p = jedis.pipelined();
+           m.forEach((key,value) -> {
+               p.hset(mapId, key, value);
+           });
+           p.sync();
+       }
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.del(mapId);
+        }
     }
 
     @Override
     public Set<String> keySet() {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()) {
+            return jedis.hkeys(mapId);
+        }
     }
 
     @Override
     public Collection<String> values() {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()){
+            return jedis.hvals(mapId);
+        }
     }
 
     @Override
     public Set<Entry<String, String>> entrySet() {
-        throw new UnsupportedOperationException();
+        try (Jedis jedis = jedisPool.getResource()){
+            return new HashSet<>(jedis.hgetAll(mapId).entrySet());
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable{
+        System.out.println("Try to delete map");
+        try (Jedis jedis = jedisPool.getResource()){
+            jedis.hincrBy("usedMap", mapId, -1);
+
+            if (jedis.hget("usedMap", mapId).equals("0")) {
+                jedis.del(mapId);
+                jedis.hdel("usedMap", mapId);
+            }
+        }finally {
+            super.finalize();
+        }
     }
 }
